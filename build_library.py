9d854ca8a9ca060930481fd7605c685a0286d9d8
agustin <agustin.piqueres@gmail.com>
2022-09-22 22:33:53 +0200

    feat: functions to create item list and wrap details in markdown

--- build_library.py
+++ build_library.py
@@ -2,28 +2,54 @@
 
 import glob
 import pathlib
+from typing import *
 
 from jinja2 import Environment, FileSystemLoader
 
-environment = Environment(loader=FileSystemLoader("templates/"))
-template = environment.get_template("template_entrada.md")
-print(template)
+TEMPLATE_ENTRY = "entrada_alumno.md_t"
+TEMPLATE_README = "README.md_t"
 
-ejemplo = {
-    "autor": "hey",
-    "tutor": "",
-    "resumen": 1,
-    "memoria": 1,
-    "autor": 1,
-    "autor": 1,
+
+def itemize(elements: List[str]) -> str:
+    """Transforms a list of str to an item list in markdown format."""
+    items = ""
+    if elements == [""] or elements == []:
+        return ""
+    for el in elements:
+        items += f"- {el}\n"
+    return items
+
+
+def wrap_detailed(end_year: str = "", text: str = "") -> str:
+    """Wraps text in html so the text appears clickable. """
+    return (
+        "<details>"
+        f"<summary> Promoción: {end_year} </summary>"
+        "<hr>"
+
+        f"{text}"
+
+        "</details>"
+    )
+
+
+environment = Environment(loader=FileSystemLoader("templates/"))
+template = environment.get_template(TEMPLATE_ENTRY)
+
+example = {
+    "author": "author",
+    "title": "title",
+    "tutors": "tutor1, tutor2",
+    "abstract": "abstract",
+    "fields": "field1, field2",
+    "memory_link": "memory",
+    "repository_links": "link1, link2",
+    "email": "email",
+    "gh_user": "gh_user",
 }
 
-content = template.render(
-    student,
-    max_score=max_score,
-    test_name=test_name
-)
-
+entry_content = template.render(**example)
+print(entry_content)
 # Para localizar las nuevas entradas
 works = [pathlib.Path(p) for p in glob.glob("./trabajos/**/*.md", recursive=True)]
-#print(f"Paths: {works}")
+# print(f"Paths: {works}")
