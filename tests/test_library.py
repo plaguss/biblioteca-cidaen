c34f54065a69bccc3099dbfb4f99bc4d6cb687fc
agustin <agustin.piqueres@gmail.com>
2022-09-22 22:50:21 +0200

    test: passing for item lists

--- tests/test_library.py
+++ tests/test_library.py
@@ -1,4 +1,5 @@
 import pytest
+from jinja2 import Environment, FileSystemLoader
 
 import build_library as bl
 
@@ -11,5 +12,49 @@ def test_itemize():
 
 
 def test_wrap_detailed():
-    text = '<details><summary> Promoción: 2022 </summary><hr>this text</details>'
+    text = "<details><summary> Promoción: 2022 </summary><hr>this text</details>"
     assert text == bl.wrap_detailed(end_year="2022", text="this text")
+
+
+def test_transform_strlist():
+    assert ["link1", "link2"] == bl._transform_str_list("link1, link2")
+    assert "" == bl._transform_str_list("")
+    assert ["l1", "l2", "l3"] == bl._transform_str_list("l1,l2,,l3")
+
+
+def test_itemize_field():
+    example = {
+        "author": "author",
+        "title": "title",
+        "tutors": "tutor1, tutor2",
+        "abstract": "abstract",
+        "fields": "field1, field2",
+        "memory_link": "memory",
+        "repository_links": "link1, link2",
+        "email": "email",
+        "gh_user": "gh_user",
+    }
+    tutors = bl.itemize_field(example["tutors"])
+    assert tutors == "- tutor1\n- tutor2\n"
+    links = bl.itemize_field(example["repository_links"])
+    assert links == "- link1\n- link2\n"
+
+
+def test_fill_student_entry():
+
+    example = {
+        "author": "author",
+        "title": "title",
+        "tutors": "tutor1, tutor2",
+        "abstract": "abstract",
+        "fields": "field1, field2",
+        "memory_link": "memory",
+        "repository_links": "link1, link2",
+        "email": "email",
+        "gh_user": "gh_user",
+    }
+    environment = Environment(loader=FileSystemLoader("templates/"))
+    template = environment.get_template(bl.TEMPLATE_ENTRY)
+
+    entry_content = template.render(**example)
+    assert entry_content == 2
