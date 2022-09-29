51e935997f2119928d70a10e785db08b2587fbaa
agustin <agustin.piqueres@gmail.com>
2022-09-29 19:53:00 +0200

    fix: updated build readme to avoid running git if nothing changed

--- noxfile.py
+++ noxfile.py
@@ -10,12 +10,17 @@ $ nox --session build_readme
 
 """
 
-import nox
+import subprocess
 from pathlib import Path
+from unittest import result
+
+import nox
 
 
 def install_dev_requirements(session):
-    session.run("pip", "install", "-r", str(Path("requirements") / "dev_requirements.txt"))
+    session.run(
+        "pip", "install", "-r", str(Path("requirements") / "dev_requirements.txt")
+    )
 
 
 def install_requirements(session):
@@ -24,14 +29,24 @@ def install_requirements(session):
 
 @nox.session
 def build_readme(session):
-    """Installs the requirements, recreates the main README and pushes it to the main branch. """
+    """Installs the requirements, recreates the main README and pushes it to the main branch."""
     install_requirements(session)
     session.run("python", "build_library.py", "-l")
-    session.run("git", "config", "user.name", "agus", external=True)
-    session.run("git", "config", "user.email", "agustin.piqueres@gmail.com", external=True)
-    session.run("git", "add", "README.md", external=True)
-    session.run("git", "commit", "-m", "autocommit from within nox", external=True)
-    session.run("git", "push", "upstream", "main", external=True)
+    # Check if the README.md has changed at all, otherwise
+    # stop running git to avoid failing the workflow
+    result = subprocess.run(
+        ["git", "diff", "README.md"], capture_output=True, encoding="utf-8"
+    ).stdout
+    if result == "":
+        print("README.md didn't changed, that's it.")
+    else:
+        session.run("git", "config", "user.name", "agus", external=True)
+        session.run(
+            "git", "config", "user.email", "agustin.piqueres@gmail.com", external=True
+        )
+        session.run("git", "add", "README.md", external=True)
+        session.run("git", "commit", "-m", "autocommit from within nox", external=True)
+        session.run("git", "push", "upstream", "main", external=True)
 
 
 @nox.session(python=["3.8", "3.9", "3.10"])
